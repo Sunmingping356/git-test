43ffef7a268ec04a96b4b7c72e980ceceef1cbdd
thirteensun <1668067692@qq.com>
2022-07-31 16:48:53 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author 十三
+ * @create 2022-07-31 16:19
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
